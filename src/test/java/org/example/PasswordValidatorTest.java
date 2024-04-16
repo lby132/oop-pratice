c34090f8b0dbd8cd4248d279012bd9427ccb7ce6
lby132 <quddbscjswow@naver.com>
2024-04-16 23:42:42 +0900

    Initial commit

---
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class PasswordValidatorTest {
+
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
+    @Test
+    void validatePasswordTest() {
+        // PasswordValidator.validate() 코드를 실행했을때 doesNotThrowAnyException 어떤 익셉션도 발생하지 않는다.
+        assertThatCode(() -> PasswordValidator.validate("serverwizard"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @Test
+    void validatePasswordTest2() {
+        assertThatCode(() -> PasswordValidator.validate("aabb"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+    }
+}
