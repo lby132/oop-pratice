affb836b657c07cbbb169b99679648c9888c250e
lby132 <quddbscjswow@naver.com>
2024-04-22 23:23:22 +0900

    음식점 구현하기

--- src/test/java/org/example/restaurant/CustomerTest.java
+++ src/test/java/org/example/restaurant/CustomerTest.java
@@ -1,5 +1,12 @@
 package org.example.restaurant;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 /**
  * 음식점에서 음식 주문하는 과정 구현
  * 요구사항
@@ -19,4 +26,15 @@ package org.example.restaurant;
  * 6. 구현하기
  */
 public class CustomerTest {
+
+    @DisplayName("메뉴이름에 해당하는 요리를 주문한다.")
+    @Test
+    void orderTest() {
+        final Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈가스", 5000), new MenuItem("냉면", 7000)));
+        final Cooking cooking = new Cooking();
+
+        assertThatCode(() -> customer.order("돈가스", menu, cooking))
+                .doesNotThrowAnyException();
+    }
 }
